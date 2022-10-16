baf4232729b3e8ec14e6c1bb508c89f8b43dc755
Abdul Hannan <abdulhannan8540680@gmail.com>
2022-10-16 17:35:26 +0500

    completed chat functionality

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -19,4 +19,9 @@ io.on('connection', socket =>{
         socket.broadcast.emit('receive', {message: message, name: users[socket.id]});
     });
 
+    socket.on('disconnect', name =>{
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
+
 });
--- js/client.js
+++ js/client.js
@@ -4,30 +4,39 @@ const form = document.getElementById('send');
 const msg_input = document.getElementById('msgInput');
 const msg_container = document.querySelector('.container');
 
+let audio = new Audio('noti.mp3');
+
 const append = (message, status)=>{
     const  messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message');
     messageElement.classList.add(status);
     msg_container.append(messageElement);
+    if (status == 'received'){
+        audio.play();
+    }
 };
-
-const sendMessage = (e)=>{
-    e.preventDefault();
-    const message = msg_input.value;
-    append(`You: ${message}`);
-}
 
 const name = prompt("Enter your name to join");
 
 socket.emit('new-user-joined', name);
 
-form.addEventListener('submit', sendMessage());
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = msg_input.value;
+    append(`You: ${message}`, 'sent');
+    socket.emit('send', message);
+    msg_input.value = "";
+});
 
 socket.on('user-joined', name => {
-    append(`${name} joined the chat`, 'joined');
+    append(`${name} joined the chat`, 'alert');
 });
 
 socket.on('receive', data => {
-    append(`${data.message} : ${data.user}`, 'received');
+    append(`${data.name} : ${data.message}`, 'received');
+});
+
+socket.on('left', name => {
+    append(`${name} left the chat`, 'alert');
 });
