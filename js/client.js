f70271de1b71053867254db339294cfcb109913d
Abdul Hannan <abdulhannan8540680@gmail.com>
2022-10-16 18:16:44 +0500

    added scroll to bottom on message sent

--- js/client.js
+++ js/client.js
@@ -7,14 +7,19 @@ const msg_container = document.querySelector('.container');
 let audio = new Audio('noti.mp3');
 
 const append = (message, status)=>{
+    
     const  messageElement = document.createElement('div');
     messageElement.innerText = message;
     messageElement.classList.add('message');
     messageElement.classList.add(status);
     msg_container.append(messageElement);
+
+    msg_container.scrollTo(0, msg_container.scrollHeight);
+
     if (status == 'received'){
         audio.play();
     }
+
 };
 
 const name = prompt("Enter your name to join");
